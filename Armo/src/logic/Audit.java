452aff53f00156a9eb1e372fb08aebaa6d86ea86
Estudiante <Estudiante@DESKTOP-GJ7E775>
2016-03-30 12:51:38 -0500

    initial version

---
+++ Armo/src/logic/Audit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Audit {
+    private String idProcess;
+    private int operation;   //new, delete, update  
+    private Date dateAudit;
+    private String userName;
+            
+    
+}
